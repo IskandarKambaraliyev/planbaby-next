52adbb644203608d1c3c2412db998b0f5d16fa79
Iskandar Kambaraliyev <human.github@gmail.com>
2025-06-26 00:59:01 +0500

    improve seo of the store and cart pages

--- app/[locale]/cart/page.tsx
+++ app/[locale]/cart/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
+import { type Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
 import StoredSection from "@/components/section/cart/Section";
 import StoreSection from "@/components/section/home/store/Section";
 import { StoreSkeleton } from "@/components/skeleton";
-import { Suspense } from "react";
 
 export default async function CartPage() {
   return (
@@ -15,4 +18,18 @@ export default async function CartPage() {
       </section>
     </>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+
+  return {
+    title: t("cartPage.meta.title"),
+    description: t("cartPage.meta.description"),
+    keywords: t("cartPage.meta.keywords"),
+    openGraph: {
+      title: t("cartPage.meta.title"),
+      description: t("cartPage.meta.description"),
+    },
+  };
 }
--- app/[locale]/store/page.tsx
+++ app/[locale]/store/page.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from "react";
+import { type Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
 import StoreSection from "@/components/section/home/store/Section";
 import { StoreSkeleton } from "@/components/skeleton";
@@ -13,4 +15,18 @@ export default async function StorePage() {
       </section>
     </>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+
+  return {
+    title: t("storePage.meta.title"),
+    description: t("storePage.meta.description"),
+    keywords: t("storePage.meta.keywords"),
+    openGraph: {
+      title: t("storePage.meta.title"),
+      description: t("storePage.meta.description"),
+    },
+  };
 }
